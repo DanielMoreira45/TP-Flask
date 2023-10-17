6e144d51f4326ae3e19aed33b3feb0d4c60d79fc
AdeleBrion <adeel.brion@etu.univ-orleans.fr>
2023-10-17 16:09:10 +0200

    ajustement

--- app/views.py
+++ app/views.py
@@ -1,11 +1,11 @@
 from .app import app
 from flask import render_template
-from .models import get_sample
+from .models import get_sample, get_sample2
 
 @app.route("/")
 def home():
     return render_template(
         "home.html", 
         title="My Anime !", 
-        animes=get_sample()
+        animes=get_sample2()
     )
